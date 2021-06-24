9684077c1f4343837693172c71460f3de5846d9e
Rajat1192 <prajat21@gmail.com>
2021-06-24 16:15:43 +0530

    update addon list grid view layout

--- templates/xt-facebook-events-support.php
+++ templates/xt-facebook-events-support.php
@@ -44,52 +44,51 @@ $twitter_url = 'https://twitter.com/XylusThemes/';
             }
         }
         ?>
-        <div class="" style="margin-top: 20px;">
-            <h3 class="setting_bar"><?php _e( 'Plugins you should try','xt-facebook-events' ); ?></h3>
-            <?php 
-            if( !empty( $plugins ) ){
-                foreach ($plugins as $plugin ) {
-                    ?>
-                    <div class="plugin_box">
-                        <?php if( $plugin->banners['low'] != '' ){ ?>
-                            <img src="<?php echo $plugin->banners['low']; ?>" class="plugin_img" title="<?php echo $plugin->name; ?>">
-                        <?php } ?>                    
-                        <div class="plugin_content">
-                            <h3><?php echo $plugin->name; ?></h3>
-
-                            <?php wp_star_rating( array(
-                            'rating' => $plugin->rating,
-                            'type'   => 'percent',
-                            'number' => $plugin->num_ratings,
-                            ) );?>
-
-                            <?php if( $plugin->version != '' ){ ?>
-                                <p><strong><?php _e( 'Version:','xt-facebook-events' ); ?> </strong><?php echo $plugin->version; ?></p>
-                            <?php } ?>
-
-                            <?php if( $plugin->requires != '' ){ ?>
-                                <p><strong><?php _e( 'Requires:','xt-facebook-events' ); ?> </strong> <?php _e( 'WordPress ','xt-facebook-events' ); echo $plugin->requires; ?>+</p>
-                            <?php } ?>
-
-                            <?php if( $plugin->active_installs != '' ){ ?>
-                                <p><strong><?php _e( 'Active Installs:','xt-facebook-events' ); ?> </strong><?php echo $plugin->active_installs; ?>+</p>
-                            <?php } ?>
-
-                            <?php //print_r( $plugin ); ?>
-                            <a class="button button-secondary" href="<?php echo admin_url( 'plugin-install.php?tab=plugin-information&plugin='. $plugin->slug.'&TB_iframe=1&width=772&height=600'); ?>" target="_blank">
-                                <?php _e( 'Install Now','xt-facebook-events' ); ?>
-                            </a>
-                            <a class="button button-primary" href="<?php echo $plugin->homepage . '?utm_source=crosssell&utm_medium=web&utm_content=supportpage&utm_campaign=freeplugin'; ?>" target="_blank">
-                                <?php _e( 'Buy Now','xt-facebook-events' ); ?>
-                            </a>
-                        </div>
-                    </div>
-                    <?php
-                }
-            }
-            ?>
-            <div style="clear: both;">
-        </div>
+        <h3 class="setting_bar"><?php _e( 'Plugins you should try', 'xt-facebook-events' ); ?></h3>
+		<div id="xtfe-addons-list">
+			<?php
+			if ( ! empty( $plugins ) ) {
+				foreach ( $plugins as $plugin ) {
+				
+					$plugin_activation = is_plugin_active( $plugin->slug.'/'. $plugin->slug.'.php' );
+					$plugin_not_active = ABSPATH . 'wp-content/plugins/'.$plugin->slug.'/';
+					$buy_now = "<a class='xtfe-status-download button-primary' target='_blank' href='".$plugin->homepage."'>Buy Now</a>";                  
+				?>
+					<div class="xtfe-addon-container">
+						<div class="xtfe-addon-item">
+							<div class="xtfe-details xtfe-clear" style="height: 165px;">
+								<img src="<?php echo $plugin->icons['2x']; ?>">
+								<h5 class="xtfe-addon-name"><?php echo $plugin->name; ?></h5>
+								<p class="xtfe-addon-desc"><?php echo $plugin->short_description; ?></p>
+							</div>
+							<div class="actions xtfe-clear">
+								<div class="xtfe-status">
+									<strong>
+									<?php _e( 'Active Installs: ', 'xt-facebook-events' ); ?><span class="xtfe-status-label xtfe-status-download"><?php echo $plugin->active_installs; ?>+</span></strong>
+								</div>
+								<div class="xtfe-action-button">
+									
+									<?php add_thickbox(); ?>
+									<?php if( $plugin_activation == true ){ ?>
+										<a class="xtfe-status-download button-secondary" disabled ><?php _e( 'Actived', 'xt-facebook-events' ); ?> </a>
+										<?php echo $buy_now; ?>
+									<?php }elseif( is_dir( $plugin_not_active ) && $plugin_activation == false ){ ?>
+										<a class="xtfe-status-download button-secondary"  href="<?php echo admin_url( 'plugins.php' ); ?>" ><?php _e( 'Activate', 'xt-facebook-events' ); ?></a>
+										<?php echo $buy_now; ?>
+									<?php }else{ ?>
+										<a class="xtfe-status-download button button-secondary thickbox" href="<?php echo admin_url( 'plugin-install.php?tab=plugin-information&plugin=' . $plugin->slug . '&TB_iframe=true&width=772&height=600' ); ?>" >
+									<?php _e( 'Install Plugin', 'xt-facebook-events' ); ?></a>
+									<?php echo $buy_now; } ?>
+								</div>
+							</div>
+						</div>
+					</div>
+				<?php
+				}
+			}
+			?>
+			</div>
+		<div style="clear: both;">
     </div>
 
 </div>
