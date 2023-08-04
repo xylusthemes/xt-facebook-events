8420f53966f2dfcf979cdc50b6b573203b4e1f6b
Rajat Patel <prajat21@gmail.com>
2023-08-04 18:27:09 +0530

    added shortcodes and widgets tab

--- assets/js/xt-facebook-events-admin.js
+++ assets/js/xt-facebook-events-admin.js
@@ -4,3 +4,26 @@ jQuery(document).ready(function($){
         $(this).wpColorPicker();
     });
 });
+
+//Shortcode Copy Text
+jQuery(document).ready(function($){
+    $(document).on("click", ".xtfe-btn-copy-shortcode", function() { 
+        var trigger = $(this);
+        $(".xtfe-btn-copy-shortcode").removeClass("text-success");
+        var $tempElement = $("<input>");
+        $("body").append($tempElement);
+        var copyType = $(this).data("value");
+        $tempElement.val(copyType).select();
+        document.execCommand("Copy");
+        $tempElement.remove();
+        $(trigger).addClass("text-success");
+        var $this = $(this),
+        oldText = $this.text();
+        $this.attr("disabled", "disabled");
+        $this.text("Copied!");
+        setTimeout(function(){
+            $this.text("Copy");
+            $this.removeAttr("disabled");
+        }, 800);
+    });
+});
