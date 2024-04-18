a319047c0de74ed93ea51f05135581da2c91ff4a
Rajat Patel <prajat21@gmail.com>
2024-04-18 16:11:52 +0530

    Change facebook api version

--- includes/class-xt-facebook-events-fb-authorize.php
+++ includes/class-xt-facebook-events-fb-authorize.php
@@ -35,14 +35,14 @@ class XT_Facebook_Events_FB_Authorize {
 			$app_id = isset( $xtfe_options['facebook_app_id'] ) ? $xtfe_options['facebook_app_id'] : '';
 			$app_secret = isset( $xtfe_options['facebook_app_secret'] ) ? $xtfe_options['facebook_app_secret'] : '';
 			$redirect_url = admin_url( 'admin-post.php?action=xtfe_facebook_authorize_callback' );
-			$api_version = 'v15.0';
+			$api_version = 'v19.0';
 			$param_url = urlencode($redirect_url);
 			$xtfe_session_state = md5(uniqid(rand(), TRUE));
 			setcookie("xtfe_session_state", $xtfe_session_state, "0", "/");
 
 			if( $app_id != '' && $app_secret != '' ){
 
-				$dialog_url = "https://www.facebook.com/" . $api_version . "/dialog/oauth?client_id=" . $app_id . "&redirect_uri=" . $param_url . "&state=" . $xtfe_session_state . "&scope=groups_access_member_info,user_events,pages_show_list,pages_manage_metadata,pages_read_engagement,pages_read_user_content";
+				$dialog_url = "https://www.facebook.com/" . $api_version . "/dialog/oauth?client_id=" . $app_id . "&redirect_uri=" . $param_url . "&state=" . $xtfe_session_state . "&scope=pages_show_list,pages_manage_metadata,pages_read_engagement,pages_read_user_content,page_events";
 				header("Location: " . $dialog_url);
 
 			}else{
@@ -66,7 +66,7 @@ class XT_Facebook_Events_FB_Authorize {
 			$app_id = isset( $xtfe_options['facebook_app_id'] ) ? $xtfe_options['facebook_app_id'] : '';
 			$app_secret = isset( $xtfe_options['facebook_app_secret'] ) ? $xtfe_options['facebook_app_secret'] : '';
 			$redirect_url = admin_url('admin-post.php?action=xtfe_facebook_authorize_callback');
-			$api_version = 'v15.0';
+			$api_version = 'v19.0';
 			$param_url = urlencode($redirect_url);
 
 			if( $app_id != '' && $app_secret != '' ){
